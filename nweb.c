ed97c943d59905ee020284f404204fdee8de5bc1
Gaspard Beernaert <gaspard@gmail.com>
2018-01-21 15:08:01 +0100

    version 25

--- nweb.c
+++ nweb.c
@@ -9,28 +9,23 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#define VERSION 24
+#define VERSION 25
 #define BUFSIZE 65536 /* larger = more efficient */
 #define ERROR      42
 #define LOG        44
 #define FORBIDDEN 403
 #define NOTFOUND  404
-
+#ifndef SIGCLD
+#define SIGCLD SIGCHLD
+#endif
 struct {
 	char *ext;
 	char *filetype;
 } extensions [] = {
-	{"gif", "image/gif" },  
-	{"jpg", "image/jpg" }, 
-	{"jpeg","image/jpeg"},
-	{"png", "image/png" },  
-	{"ico", "image/ico" },  
-	{"zip", "image/zip" },  
-	{"gz",  "image/gz"  },  
-	{"tar", "image/tar" },  
-	{"htm", "text/html" },  
-	{"html","text/html" },  
-	{0,0} };
+	{"gif", "image/gif" },  {"jpg", "image/jpg" }, {"jpeg","image/jpeg"},
+	{"png", "image/png" },  {"ico", "image/ico" },  {"zip", "image/zip" },
+	{"gz",  "image/gz"  },  {"tar", "image/tar" },  {"htm", "text/html" },
+	{"html","text/html" },  {0,0} };
 
 void logger(int type, char *s1, char *s2, int socket_fd)
 {
@@ -38,22 +33,22 @@ void logger(int type, char *s1, char *s2, int socket_fd)
 	char logbuffer[BUFSIZE*2];
 
 	switch (type) {
-	case ERROR: (void)sprintf(logbuffer,"ERROR: %s:%s Errno=%d exiting pid=%d",s1, s2, errno,getpid()); 
+	case ERROR: (void)sprintf(logbuffer,"ERROR: %s:%s Errno=%d exiting pid=%d",s1, s2, errno,getpid());
 		break;
-	case FORBIDDEN: 
+	case FORBIDDEN:
 		(void)write(socket_fd, "HTTP/1.1 403 Forbidden\nContent-Length: 185\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>403 Forbidden</title>\n</head><body>\n<h1>Forbidden</h1>\nThe requested URL, file type or operation is not allowed on this simple static file webserver.\n</body></html>\n",271);
-		(void)sprintf(logbuffer,"FORBIDDEN: %s:%s",s1, s2); 
+		(void)sprintf(logbuffer,"FORBIDDEN: %s:%s",s1, s2);
 		break;
-	case NOTFOUND: 
+	case NOTFOUND:
 		(void)write(socket_fd, "HTTP/1.1 404 Not Found\nContent-Length: 136\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>404 Not Found</title>\n</head><body>\n<h1>Not Found</h1>\nThe requested URL was not found on this server.\n</body></html>\n",224);
-		(void)sprintf(logbuffer,"NOT FOUND: %s:%s",s1, s2); 
+		(void)sprintf(logbuffer,"NOT FOUND: %s:%s",s1, s2);
 		break;
 	case LOG: (void)sprintf(logbuffer," INFO: %s:%s:%d",s1, s2,socket_fd); break;
-	}	
+	}
 	/* No checks here, nothing can be done with a failure anyway */
 	if((fd = open("nweb.log", O_CREAT| O_WRONLY | O_APPEND,0644)) >= 0) {
-		(void)write(fd,logbuffer,strlen(logbuffer)); 
-		(void)write(fd,"\n",1);      
+		(void)write(fd,logbuffer,strlen(logbuffer));
+		(void)write(fd,"\n",1);
 		(void)close(fd);
 	}
 	if(type == ERROR || type == NOTFOUND || type == FORBIDDEN) exit(3);
@@ -155,7 +150,7 @@ int main(int argc, char **argv)
 		(void)printf("ERROR: Bad top directory %s, see nweb -?\n",argv[2]);
 		exit(3);
 	}
-	if(chroot(argv[2]) == -1){ 
+	if(chroot(argv[2]) == -1){
 		(void)printf("ERROR: Can't Change to directory %s\n",argv[2]);
 		exit(4);
 	}
